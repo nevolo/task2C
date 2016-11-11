afa1cc367120a2c26f64f2b9d663086804f99334
Aleksandr <kazadoev.aleksandr@mail.ru>
2016-11-11 21:23:45 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -9,11 +9,22 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/task2A', (req, res) => {
-  const sum = (+req.query.a || 0) + (+req.query.b || 0);
-  res.send(sum.toString());
+app.get('/task2C', (req, res) => {
+  const fullname = req.query.username;  //запрос вида "https://vk.com/durov"
+  const re = new RegExp(/(https?:)?(\/\/)?(www\.)?(.+?\/)?(@)?(\w+)?(\.)?(\w+)?/);  //регулярное выражение
+  const username = fullname.match(re);  //ищем совпадения, возвращается массив совпавших частей
+  console.log(username);
+
+  var tmp1 = username[username.length-1]; 
+  var tmp2 = username[username.length-2];
+
+  res.send('@' + username[username.length-3] + (tmp2 ? tmp2 : "" ) + (tmp1 ? tmp1 : "" )); 
 });
 
 app.listen(3000, () => {
   console.log('Your app listening on port 3000!');
 });
+
+
+
+   //const re = new RegExp('(https?:)?(\/\/)?((telegram|vk|vkontakte|twitter|github)[^\/]*\/)([a-zA-Z0-9а-яА-Я]*).([a-zA-Z0-9а-яА-Я]*)');
